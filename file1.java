c83a624ab9a59ae9c66b267bc43b1689dd01893e
cubeiplkumar <trainings.anil@gmail.com>
2021-08-13 02:34:13 +0530

    his-301 related changes

--- file1.java
+++ file1.java
@@ -5,6 +5,11 @@ public class HelloWorld {
     Greeter greeter = new Greeter();
     System.out.println(greeter.sayHello());
   }
+  private Map<String,Object > cache= new HashMap<String,Object>()
   LocalTime currentTime = new LocalTime();
     System.out.println("The current local time is: " + currentTime)
+	//HIS-301 changes
+	public void loadDataToCache(){
+		//logic
+	}
 }
